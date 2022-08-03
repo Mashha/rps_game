13a8b0bbf89a5142c8a1035d955e20eb154c2941
Masa Orel <orel.masha89@gmail.com>
2022-08-03 14:29:25 +0200

    made upgrades

--- app.js
+++ app.js
@@ -1,66 +1,88 @@
-//console game
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
+
+//buttons
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors")
+
+// result of each match
+let result = document.getElementById("result")
+
+//final result
+let finalResult = document.querySelector(".finalResult")
 
 //score
 let computerScore = 0;
 let userScore = 0;
-// random computer choice
 
+//score output 
+let computerSpanScore = document.getElementById("computer-score");
+let userSpanScore = document.getElementById("user-score");
+
+// computer chose
+let compSpanChoice = document.getElementById("comp")
+
+// random computer choice
 const options = ["rock", "paper", "scissors"];
 
-//random computer choice
 function computerPlay() {
    return options[Math.floor(Math.random() * options.length)]
 }
+
 let computerSelection = computerPlay()
 
+// event listeners to buttons
+rock.addEventListener("click", () => {
+    playGame("rock", computerPlay())
+    
+})
+paper.addEventListener("click", () => {
+    playGame("paper", computerPlay())
+    
+})
+scissors.addEventListener("click", () => {
+    playGame("scissors", computerPlay())
+    
+})
 
-// game with computer
+// event listener for reset button
+let resetButton = document.getElementById("reset")
+resetButton.addEventListener("click", restart)
+
+//game with computer
 function playGame(playerSelection, computerSelection) {
-    if(playerSelection.toLowerCase() === "rock" && computerSelection === "scissors" || playerSelection.toLowerCase() === "scissors" && computerSelection === "paper" || playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+    if(playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
         userScore++;
-        return `Yaay you won! ${playerSelection} beats ${computerSelection}`;
+        result.textContent = `Yaay you won! ${playerSelection} beats ${computerSelection}`;
         // user wins
-    } else if( playerSelection.toLowerCase() === "rock" && computerSelection === "paper" || playerSelection.toLowerCase() === "scissors" && computerSelection === "rock" || playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+    } else if( playerSelection === "rock" && computerSelection === "paper" || playerSelection === "scissors" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "scissors") {
         computerScore++;
         // computer wins
-        return `You lost :( ${computerSelection} beats ${playerSelection}`;
-    } else if (playerSelection.toLowerCase() === computerSelection) {
-        return "It's a tie";
+        result.textContent = `You lost :( ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection === computerSelection) {
+        result.textContent = "It's a tie";
     } else {
-        return "Invalid input";
+        result.textContent = "Invalid input";
     }
-    
+    compSpanChoice.textContent = `${computerSelection}`
+    computerSpanScore.textContent = `${computerScore}` 
+    userSpanScore.textContent = `${userScore}`
+    win()
 }
-
-// five rounds with computer
-function round() {
-    for(let i = 0; i < 5; i++) {
-    let playerSelection = prompt("what do you choose");
-
-    console.log(`Computer chose: ${computerSelection}`)
-    console.log(`Player chose: ${playerSelection}`)
-    
-    console.log(playGame(playerSelection, computerSelection))
-
-    console.log(`Computer score: ${computerScore} and user score: ${userScore}`)
-    
-    }
-    console.log(win())
-
-}
-
-round()
 
 
 function win() {
-    if(userScore > computerScore) {
-        return "You won the round :)"
-    } else if(computerScore > userScore) {
-        return "You lost this round :("
-    } else {
-        return "It's a tie, play one more"
-    }
+    if(userScore === 5) {
+        finalResult.textContent = "You won the round :)"
+    } else if(computerScore === 5) {
+        finalResult.textContent = "You lost this round :("
+    } 
+}
+
+// restart button
+function restart() {
+    compSpanChoice.textContent = "";
+    result.textContent = "";
+    finalResult.textContent = "";
+    userSpanScore.textContent = `${0}`
+    computerSpanScore.textContent = `${0}` 
 }
